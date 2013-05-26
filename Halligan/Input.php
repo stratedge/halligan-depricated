edc482f3fe0f51a5bcf44f5ca491383b981a54d1
xjstratedgebx <jarret@solvomedia.com>
2013-05-26 11:40:33 -0400

    Adding an Input class to get data sent from the browser in the GET, POST, and REQUEST super global variables.

---
+++ Halligan/Input.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Halligan;
+
+class Input {
+
+	public static function post($key, $trim = TRUE)
+	{
+		if(isset($_POST[$key]) && !empty($_POST[$key]))
+		{
+			return $trim ? trim($_POST[$key]) : $_POST[$key];
+		}
+
+		return FALSE;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+
+
+	public static function allPost($trim = TRUE)
+	{
+		$post = array();
+
+		foreach($_POST as $key => $value)
+		{
+			$post[$key] = static::post($key, $trim);
+		}
+
+		return $post;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public static function get($key, $trim = TRUE)
+	{
+		if(isset($_GET[$key]) && !empty($_GET[$key]))
+		{
+			return $trim ? trim($_GET[$key]) : $_GET[$key];
+		}
+
+		return FALSE;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+
+
+	public static function allGet($trim = TRUE)
+	{
+		$get = array();
+
+		foreach($_GET as $key => $value)
+		{
+			$get[$key] = static::get($key, $trim);
+		}
+
+		return $get;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+	
+
+	public static function request($key, $trim = TRUE)
+	{
+		if(isset($_REQUEST[$key]) && !empty($_REQUEST[$key]))
+		{
+			return $trim ? trim($_REQUEST[$key]) : $_REQUEST[$key];
+		}
+
+		return FALSE;
+	}
+
+
+	//---------------------------------------------------------------------------------------------
+
+
+	public static function allRequest($trim = TRUE)
+	{
+		$request = array();
+
+		foreach($_REQUEST as $key => $value)
+		{
+			$request[$key] = static::request($key, $trim);
+		}
+
+		return $request;
+	}
+}
+
+/* End of file Input.php */
+/* Location: ./Halligan/Input.php */
